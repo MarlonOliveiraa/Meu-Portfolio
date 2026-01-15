d099b4d27771d0ccd921bd48fe48afc035905c49
MarlonOliveiraa <marlonsantanaoliveira2007@gmail.com>
2026-01-14 22:11:16 -0400

    fix: set base path for production in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/Meu-Portfolio/" : "/",
+  base: '/Meu-Portfolio/',
   server: {
     host: "::",
     port: 8080,
