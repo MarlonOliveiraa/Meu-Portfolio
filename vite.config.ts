272d535b8728a7d358be51112e0dc5417df19e1c
MarlonOliveiraa <marlonsantanaoliveira2007@gmail.com>
2026-01-20 21:58:11 -0400

    Refactor code structure for improved readability and maintainability

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path"
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Meu-Portfolio/",
+  base: "./", // importante! relativo à raiz
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
